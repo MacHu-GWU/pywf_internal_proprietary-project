fa32148ed384c5a7565027a34e89dc4078e960ed
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-04 11:52:54 -0400

    update test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,40 @@ def test():
     _ = api
 
     _ = api.PyWf
+    _ = api.PyWf.create_virtualenv
+    _ = api.PyWf.remove_virtualenv
+    _ = api.PyWf.poetry_source_add_codeartifact
+    _ = api.PyWf.poetry_authorization
+    _ = api.PyWf.pip_authorization
+    _ = api.PyWf.twine_authorization
+    _ = api.PyWf.poetry_lock
+    _ = api.PyWf.poetry_export
+    _ = api.PyWf.poetry_install_only_root
+    _ = api.PyWf.poetry_install
+    _ = api.PyWf.poetry_install_dev
+    _ = api.PyWf.poetry_install_test
+    _ = api.PyWf.poetry_install_doc
+    _ = api.PyWf.poetry_install_auto
+    _ = api.PyWf.poetry_install_all
+    _ = api.PyWf.run_unit_test
+    _ = api.PyWf.run_cov_test
+    _ = api.PyWf.view_cov
+    _ = api.PyWf.run_int_test
+    _ = api.PyWf.run_load_test
+    _ = api.PyWf.notebook_to_markdown
+    _ = api.PyWf.build_doc
+    _ = api.PyWf.view_doc
+    _ = api.PyWf.deploy_versioned_doc
+    _ = api.PyWf.deploy_latest_doc
+    _ = api.PyWf.view_latest_doc
+    _ = api.PyWf.create_cloudflare_pages_project
+    _ = api.PyWf.deploy_cloudflare_pages
+    _ = api.PyWf.poetry_build
+    _ = api.PyWf.publish_to_codeartifact
+    _ = api.PyWf.remove_from_codeartifact
+    _ = api.PyWf.publish_to_github_release
+    _ = api.PyWf.setup_codecov_io_upload_token_on_github
+    _ = api.PyWf.setup_cloudflare_pages_upload_token_on_github
 
 
 if __name__ == "__main__":
